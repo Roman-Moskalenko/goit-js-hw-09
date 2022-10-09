a568dc2a5e3e9c23c98a4ffd227417a7d0893427
Roman-Moskalenko <92758017+Roman-Moskalenko@users.noreply.github.com>
2022-10-09 13:13:58 +0300

    hw 9

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,24 @@
+const startBtn = document.querySelector ('[data-start]')
+const stopBtn = document.querySelector ('[data-stop]')
+const body = document.querySelector ('body')
+
+
+  startBtn.addEventListener('click',  onStartBtn)
+  stopBtn.addEventListener('click', onStopBtn)
+  stopBtn.disabled = true;
+function onStartBtn(){
+    timerId = setInterval(onBodyBackgroundColor, 1000)
+    startBtn.disabled = true
+    stopBtn.disabled = false
+}
+function onStopBtn(){
+    clearTimeout(timerId)
+    startBtn.disabled = false
+    stopBtn.disabled = true
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+function onBodyBackgroundColor(){
+    body.style.backgroundColor = getRandomHexColor();
+}

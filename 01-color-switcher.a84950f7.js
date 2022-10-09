3540d4bcd72dd839eb27bf9a14ccc719e5722314
Roman-Moskalenko <92758017+Roman-Moskalenko@users.noreply.github.com>
2022-10-09 10:14:42 +0000

    Deploying to gh-pages from @ Roman-Moskalenko/goit-js-hw-09@a568dc2a5e3e9c23c98a4ffd227417a7d0893427 🚀

---
+++ 01-color-switcher.a84950f7.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");function o(){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(function(){timerId=setInterval(o,1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearTimeout(timerId),e.disabled=!1,t.disabled=!0})),t.disabled=!0;
+//# sourceMappingURL=01-color-switcher.a84950f7.js.map
